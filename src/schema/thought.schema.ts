ec72387f1767d00367da31a669e1448d3a41228c
JulioRios15 <89167159+JulioRios15@users.noreply.github.com>
2021-12-11 19:52:01 -0400

    thought schema

--- src/schema/thought.schema.ts
+++ src/schema/thought.schema.ts
@@ -1,31 +1,38 @@
 import { object,  string, TypeOf } from "zod";
 
-const params = {
-    params: object({
-        thoughtId: string({
-            required_error: "thoughtId is required"
-        })
+const params = object({
+    thoughtId: string({
+        required_error: "thoughtId is required"
     })
-}
+});
 
-const payload = {
-    body: object({
-        thoughtText: string({
-            required_error: "thought text is required"
-        }),
-        username: string({
-            required_error: "username is required"
-        }),
-    })
-}
+const body = object({
+    thoughtText: string({
+        required_error: "thought text is required"
+    }),
+    username: string({
+        required_error: "username is required"
+    }),
+});
 
 export const createThoughtSchema = object({
-    ...payload
+    body: body
   });
 
 export const getThoughtSchema = object({
-    ...params
-})
+    params: params
+});
+
+export const updateThoughtSchema = object({
+    params: params,
+    body: body.omit({username: true})
+});
+
+export const deleteThoughtSchema = object({
+    params: params
+});
 
 export type CreateThoughtInput = TypeOf<typeof createThoughtSchema>;
 export type ReadThoughtInput = TypeOf<typeof getThoughtSchema>;
+export type UpdateThoughtSchema = TypeOf<typeof updateThoughtSchema>;
+export type DeleteThoughtSchema = TypeOf<typeof deleteThoughtSchema>;
