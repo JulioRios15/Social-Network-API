5c52402cc9a17949884b2fc819e2d7817572fdf0
JulioRios15 <89167159+JulioRios15@users.noreply.github.com>
2021-12-11 11:41:27 -0400

    user schema
    implemente user schema route validation

---
+++ src/schema/user.schema.ts
@@ -0,0 +1,55 @@
+import { object,  string, TypeOf } from "zod";
+
+const payload = {
+    body: object({
+      username: string({
+        required_error: "username is required",
+      }),
+      email: string({
+        required_error: "email is required",
+      })
+    }),
+  };
+  
+  const params = {
+    params: object({
+      userId: string({
+        required_error: "userId is required",
+      }),
+    })
+  };
+
+
+  export const createUserSchema = object({
+    body: object({
+      username: string({
+        required_error: "username is required",
+      }),
+      email: string({
+        required_error: "email is required",
+      }),
+      password: string({
+        required_error: "password is required",
+      })
+    })
+  });
+
+  export const updateUserSchema = object({
+    ...params,
+    ...payload
+  });
+
+  export const getUserSchema = object({
+    ...params
+  })
+
+  export const deleteUserSchema = object({
+    ...params
+  })
+
+  export type CreateUserInput = TypeOf<typeof createUserSchema>;
+  export type ReadUserInput = TypeOf<typeof getUserSchema>;
+  export type UpdateUserInput = TypeOf<typeof updateUserSchema>;
+  export type DeleteUserInput = TypeOf<typeof deleteUserSchema>;
+
+
