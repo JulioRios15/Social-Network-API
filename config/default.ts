e135f00b62954ad055e160b6594a7aaba8b9b4b6
JulioRios15 <89167159+JulioRios15@users.noreply.github.com>
2021-12-10 21:46:34 -0400

    update db url

--- config/default.ts
+++ config/default.ts
@@ -1,6 +1,6 @@
 export default {
     port: 1337,
-    dbUrl: "mongodb://localhost:27017/management-db",
+    dbUrl: "mongodb://localhost:27017/social-network-db",
     saltWorkFactor: 10,
     accessTokenTtl: "15m",
     refreshToken: "1y",
